90e9c6718e7dbdccfc7fda7abefbac0d558de14d
ashutosh1412 <ashutosh.sinha@ekaplus.com>
2019-04-30 15:23:27 +0530

    Deleted

--- src/main/java/kaleyra/disaster/disaster/repository/CallRepository.java
+++
@@ -1,9 +0,0 @@
-package kaleyra.disaster.disaster.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import kaleyra.disaster.disaster.DatabaseModel.CallDB;
-
-public interface CallRepository extends CrudRepository<CallDB, Integer> {
-
-}
