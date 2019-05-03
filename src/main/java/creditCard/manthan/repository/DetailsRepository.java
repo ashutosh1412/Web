3738feac35bd917b54625776641d19d2cde598b4
ashutosh1412 <ashutosh.sinha@ekaplus.com>
2019-05-04 04:39:53 +0530

    Credit Card

---
+++ src/main/java/creditCard/manthan/repository/DetailsRepository.java
@@ -0,0 +1,9 @@
+package creditCard.manthan.repository;
+
+import creditCard.manthan.dbObject.Details;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetailsRepository extends JpaRepository<Details,Integer>{
+
+}
